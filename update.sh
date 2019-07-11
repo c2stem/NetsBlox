37f2e7c9b8123f53246a0b4b23d88b7ce7d66e7f
Naveed <rmdnaveed@gmail.com>
2019-07-11 13:29:01 +0000

    changes to update shell script to update submodules

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 # This script will update the client submodule
-cd src/browser
-git fetch c2stem
-git checkout c2stem/netsblox
-npm run postinstall
-pm2 update editor --update-env
+git fetch
+git submodule update
+npm run install
+pm2 update dev
